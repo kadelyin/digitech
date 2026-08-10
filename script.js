411202a4f8c25fd1339f50e44533283129dc911e
kadelyin <23123kw@kuranuicollege.school.nz>
2026-08-11 10:57:06 +1200

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  const opening = document.getElementById("openingAnimation");
+  const banner = document.getElementById("banner");
+
+  opening.style.animationPlayState = "running";
+  banner.style.animationPlayState = "running";
+});
